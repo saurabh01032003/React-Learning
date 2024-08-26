eca6aa146381d4793e8cbf0488cdbc72e9edd5da
saurabh01032003 <saurabh858182@gmail.com>
2024-08-26 11:06:07 +0530

    Some changes done

--- Lecture-04/github-api-fetching/src/components/Memoo.jsx
+++ Lecture-04/github-api-fetching/src/components/Memoo.jsx
@@ -4,25 +4,28 @@ import { useState } from 'react'
 
 function Memoo() {
     let [count,setCount] = useState(0)
-    let [inp,setInput] = useState(0);
+    let [inp,setInput] = useState(0); // agar koi bhi cheez change ho rahi hoti hai toh uske liye hum useState banate hai
     
     function handleInput(e){
         setInput(e.target.value) // inp variable me jo value tumne input kara hai wo aa jayega
     }
     
+    // Button ke click hone pr bhi handleCount chal raha tha -> because whenever there is change in state the whole componenet rerenders
     // function handleClick(){
     //     setCount(count+1);
     // }
     // let sum = 0;
     // for(let i = 1;i <= inp;i++){ // inp tak ka sum : loop laga kar calculate kara hai
     //     sum += i;
+    //     console.log(sum);
     // }
 
-    //-----------useMemo()=> prevents rerendering
+    // //-----------useMemo()=> ek component me do props hai(count,inp) but hum chahte hai ki ek ke hi change(inp) hone pe pura component rerender ho naki dono prop(i.e inp,button) ke change hone pr
     let myAns = useMemo(()=>{
         let sum = 0;
         for(let i = 1;i <= inp;i++){ // inp tak ka sum : loop laga kar calculate kara hai
             sum += i;
+            console.log(sum);
         }
         return sum;
     },[inp]) // we want ki input(value) change hone se dubara re-calculate(not re-render) na ho, isliye dependency array me inp state dala hai
